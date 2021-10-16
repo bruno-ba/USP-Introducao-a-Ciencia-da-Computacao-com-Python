6f9bdc7e545ba81d6c384895ed4149c4031b7516
BrunoTorres <btmsba@hotmail.com>
2021-10-16 19:41:49 -0300

    deixando o exercicio mais clean

--- PARTE 1/senama 3/praticar_tarefa/ponto.py
+++ PARTE 1/senama 3/praticar_tarefa/ponto.py
@@ -1,12 +1,17 @@
+
+
 if __name__ == '__main__':
-    p1_x = int(input('Digite a coordenada x de p1: '))
-    p1_y = int(input('Digite a coordenada y de p1: '))
-    p2_x = int(input('Digite a coordenada x de p2: '))
-    p2_y = int(input('Digite a coordenada y de p2: '))
+    x_1 = int(input('Digite o valor de x1: '))
+    y_1 = int(input('Digite o valor de y1: '))
+    x_2 = int(input('Digite o valor de x2: '))
+    y_2 = int(input('Digite o valor de y2: '))
 
-    distancia = (((p1_x - p2_x)**2) + ((p1_y - p2_y)**2)) ** 0.5
+    delta = (((x_1 - x_2) ** 2) + ((y_1 - y_2) ** 2)) ** 0.5
 
-    if distancia >= 10:
+    if delta >= 10:
         print('longe')
     else:
         print('perto')
+
+
+
--- PARTE 1/senama 3/praticar_tarefa/raizes_equacao.py
+++ PARTE 1/senama 3/praticar_tarefa/raizes_equacao.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
             print(f'a raiz dupla desta equação é {raiz_x}')
         else:
             raiz_y = (-b - (delta ** 0.5)) / (2 * a)
-            if raiz_x >= raiz_y:
+            if raiz_x < raiz_y:
                 print(f'as raízes da equação são {raiz_x} e {raiz_y}')
             else:
                 print(f'as raízes da equação são {raiz_y} e {raiz_x}')
