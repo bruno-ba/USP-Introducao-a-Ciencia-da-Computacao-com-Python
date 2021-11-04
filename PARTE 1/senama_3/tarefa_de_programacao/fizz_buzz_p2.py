2522bd1b49af22a1f881d82e3aed80f5a5c24092
BrunoTorres <btmsba@hotmail.com>
2021-11-04 09:56:01 -0300

    refatorando pastas e arquivos...

--- PARTE 1/senama_3/tarefa_de_programacao/fizz_buzz_p2.py
+++
@@ -1,9 +0,0 @@
-def fizz_buzz_2(num):
-    if num % 5 == 0:
-        return 'Buzz'
-    return num
-
-
-if __name__ == '__main__':
-    num = int(input('Digite um número: '))
-    print(fizz_buzz_2(num))
--- PARTE 1/senama_3/tarefa_de_programacao/fizz_buzz_p3.py
+++
@@ -1,9 +0,0 @@
-def fizz_buzz_3(num):
-    if num % 3 ==0 and num % 5 == 0:
-        return 'FizzBuzz'
-    return num
-
-
-if __name__ == '__main__':
-    num = int(input('Digite um número: '))
-    print(fizz_buzz_3(num))
